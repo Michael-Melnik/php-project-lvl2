cbb377f8d5cffabc0a8c8810e596ff72a1f254f6
Michael_Melnik <mikhamelnbr@yandex.ru>
2022-07-11 10:45:00 +0300

    fix errors DifferTest.php and README.md

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Code Climate:
 [![Maintainability](https://api.codeclimate.com/v1/badges/d93dd9959da09ae1e481/maintainability)](https://codeclimate.com/github/Michael-Melnik/php-project-lvl2/maintainability)
-[![Test Coverage](https://codeclimate.com/github/hexlet-boilerplates/php-package/badges/coverage.svg)](https://codeclimate.com/github/hexlet-boilerplates/php-package/coverage)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/d93dd9959da09ae1e481/test_coverage)](https://codeclimate.com/github/Michael-Melnik/php-project-lvl2/test_coverage)
 
 ### Example comparison Json | Yaml (default format 'stylish')
 [![asciicast](https://asciinema.org/a/cjK0oifXMVuV1vdtIetJX182d.svg)](https://asciinema.org/a/cjK0oifXMVuV1vdtIetJX182d)
--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -15,20 +15,69 @@ class DifferTest extends TestCase
         return $this->path . $name;
     }
 
-    public function testGenDiff(): void
+//    public function testGenDiff(): void
+//    {
+//        $firstPathJson = $this->getFilePath('file1.json');
+//        $secondPathJson = $this->getFilePath('file2.json');
+//        $firstPathYaml = $this->getFilePath('file1.yaml');
+//        $secondPathYaml = $this->getFilePath('file2.yaml');
+//        $expectedStylish = trim(file_get_contents($this->getFilePath('resultStylish')));
+//        $expectedPlain = trim(file_get_contents($this->getFilePath('resultPlain')));
+//        $expectedJson = trim(file_get_contents($this->getFilePath('resultJson')));
+//        $this->assertEquals($expectedStylish, genDiff($firstPathJson, $secondPathJson, 'stylish'));
+//        $this->assertEquals($expectedStylish, genDiff($firstPathJson, $secondPathJson));
+//        $this->assertEquals($expectedStylish, genDiff($firstPathYaml, $secondPathYaml, 'stylish'));
+//        $this->assertEquals($expectedStylish, genDiff($firstPathYaml, $secondPathYaml));
+//        $this->assertEquals($expectedPlain, genDiff($firstPathYaml, $secondPathYaml, 'plain'));
+//        $this->assertEquals($expectedJson, genDiff($firstPathJson, $secondPathJson, 'json'));
+//    }
+    /**
+     * @dataProvider formatProvider
+     */
+    public function testDefault($format)
     {
-        $firstPathJson = $this->getFilePath('file1.json');
-        $secondPathJson = $this->getFilePath('file2.json');
-        $firstPathYaml = $this->getFilePath('file1.yaml');
-        $secondPathYaml = $this->getFilePath('file2.yaml');
-        $expectedStylish = trim(file_get_contents($this->getFilePath('resultStylish')));
-        $expectedPlain = trim(file_get_contents($this->getFilePath('resultPlain')));
-        $expectedJson = trim(file_get_contents($this->getFilePath('resultJson')));
-        $this->assertEquals($expectedStylish, genDiff($firstPathJson, $secondPathJson, 'stylish'));
-        $this->assertEquals($expectedStylish, genDiff($firstPathJson, $secondPathJson));
-        $this->assertEquals($expectedStylish, genDiff($firstPathYaml, $secondPathYaml, 'stylish'));
-        $this->assertEquals($expectedStylish, genDiff($firstPathYaml, $secondPathYaml));
-        $this->assertEquals($expectedPlain, genDiff($firstPathYaml, $secondPathYaml, 'plain'));
-        $this->assertEquals($expectedJson, genDiff($firstPathJson, $secondPathJson, 'json'));
+        $firstPath = $this->getFilePath("file1.{$format}");
+        $secondPath = $this->getFilePath("file2.{$format}");
+        $expected = trim(file_get_contents($this->getFilePath('resultStylish')));
+        $this->assertEquals($expected, genDiff($firstPath, $secondPath));
+    }
+    /**
+     * @dataProvider formatProvider
+     */
+    public function testJson($format)
+    {
+        $firstPath = $this->getFilePath("file1.{$format}");
+        $secondPath = $this->getFilePath("file2.{$format}");
+        $expected = trim(file_get_contents($this->getFilePath('resultJson')));
+        $this->assertEquals($expected, genDiff($firstPath, $secondPath, 'json'));
+    }
+
+    /**
+     * @dataProvider formatProvider
+     */
+    public function testPlain($format)
+    {
+        $firstPath = $this->getFilePath("file1.{$format}");
+        $secondPath = $this->getFilePath("file2.{$format}");
+        $expected = trim(file_get_contents($this->getFilePath('resultPlain')));
+        $this->assertEquals($expected, genDiff($firstPath, $secondPath, 'plain'));
+    }
+    /**
+     * @dataProvider formatProvider
+     */
+    public function testStylish($format)
+    {
+        $firstPath = $this->getFilePath("file1.{$format}");
+        $secondPath = $this->getFilePath("file2.{$format}");
+        $expected = trim(file_get_contents($this->getFilePath('resultStylish')));
+        $this->assertEquals($expected, genDiff($firstPath, $secondPath, 'stylish'));
+    }
+
+    public function formatProvider()
+    {
+        return [
+            ['json'],
+            ['yaml']
+        ];
     }
 }
